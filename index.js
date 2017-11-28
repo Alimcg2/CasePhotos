77c000408dfee7bd0b91c64742463453799da283
Alison McGuire <=>
2017-11-27 21:40:58 -0800

    lkadf

--- index.js
+++ index.js
@@ -3,18 +3,19 @@
         
         var request = new XMLHttpRequest();
         request.open("GET", "https://alimcg2.github.io/CasePhotos/testing.html");
-        request.onload = function(){
-            console.log("HI");
-            console.log(this.responseText);
-        }
+        request.onload = handleTHIS;
         request.onerror = function(){
             console.log("ooops");
         }
         request.send();
-        var time =  1000;
-        setInterval(function(){
-            time  = time + 5;
-            document.getElementById("moneyTimer").innerHTML = time;
-        }, 1000);
+
+        function handleTHIS(){
+            var time =  parseFloat(this.responseText);
+            setInterval(function(){
+                time  = time + .037;
+                document.getElementById("moneyTimer").innerHTML = time;
+            }, 1000);
+        }
+        
     }
 }());
